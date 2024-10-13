6d89a13e462cfecbffdaee5b4ede4d18ce741620
artem.artemkin <artem.artemkin@topvisor.com>
2024-10-13 15:31:42 +0300

    deserialization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import org.apache.maven.profiles.activation.SystemPropertyProfileActivator;
 import scala.Tuple2;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,32 +12,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String... args) throws IOException, IllegalAccessException {
-        //List<Integer> l = List.of(1,2,3,4,5);
-        //List<String> s = List.of("l1", "l2", "l3", "l4");
-        //HashMap<Integer, String> m = new HashMap<Integer, String>();
-        //m.put(1, "hashmap-item1");
-        //m.put(2, "hashmap-item2");
-        //m.put(3, "hashmap-item3");
-        //Person p = new Person.Builder()
-        //        .setIntVals(l)
-        //        .setStringVals(s)
-        //        .setMapVals(m)
-        //        .build();
-        //p.writeToFile("./serialized-person");
-        //FileInputStream fis = new FileInputStream("./serialized-person");
-        //byte[] bytes = fis.readAllBytes();
-        //for (byte b : bytes){
-        //       System.out.println("byte: " + b);
-        //}
-        int someshit = 1255555535;
-        byte[] serializedSomeshit = Varint.encodeInt(someshit);
-        byte[] junk = new byte[] { 123, 32, 21, 23, 40 };
-        byte[] res = new byte[serializedSomeshit.length + junk.length];
-        System.arraycopy(serializedSomeshit, 0, res, 0, serializedSomeshit.length);
-        System.arraycopy(junk, 0, res, serializedSomeshit.length, junk.length);
-        Integer deserializedSomeShit = (Integer) Varint.decodeToInt(res)._1();
-        Integer startPos = Varint.extractLength(res, 0);
-
-        System.out.println(startPos);
+        List<Integer> l = List.of(1,2,3,4,5);
+        List<String> s = List.of("l1", "l2", "l3", "l4");
+        HashMap<Integer, String> m = new HashMap<Integer, String>();
+        m.put(1, "hashmap-item1");
+        m.put(2, "hashmap-item2");
+        m.put(3, "hashmap-item3");
+        Person p = new Person.Builder()
+                .setIntVals(l)
+                .setStringVals(s)
+                .setMapVals(m)
+                .build();
+        p.writeToFile("./serialized-person");
+        FileInputStream fis = new FileInputStream("./serialized-person");
+        byte[] bytes = fis.readAllBytes();
+        for (byte b : bytes){
+               System.out.println("byte: " + b);
+        }
     }
 }
--- src/main/java/classes/Deserializer.java
+++ src/main/java/classes/Deserializer.java
@@ -1,99 +1,122 @@
 package classes;
 
 import com.github.fluency03.varint.Varint;
+
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Deserializer {
     private static final int PROTOCOL_SIGNATURE_OFFSET = 0;
     private static final int PROCOTOL_VERSION_OFFSET = PROTOCOL_SIGNATURE_OFFSET + 4;
     private static final int NULL_FIELDS_OFFSET = PROCOTOL_VERSION_OFFSET + 3;
 
-    private static String deserializeProtocolSignature(byte[] bytes, int offset){
+    private ByteBuffer bytes;
+    private String protocolSignature;
+    private List<Byte> protocolVersion;
+    private List<Byte> nullFields;
+
+    public Deserializer(byte[] bytes){
+        this.bytes = ByteBuffer.wrap(bytes);
+        this.protocolSignature = deserializeProtocolSignature();
+        this.protocolVersion = deserializeProtocolVersion();
+        this.nullFields = deserializeNullFieldPositions();
+    }
+
+    private byte nextByte(ByteBuffer bb){
+        return nextBytes(bb, 1)[0];
+    }
+
+    private byte[] nextBytes(ByteBuffer bb, int length){
+        byte[] nextBytes = new byte[length];
+        this.bytes.limit(bb.position() + length)
+                .mark()
+                .get(nextBytes);
+        return nextBytes;
+    }
+
+    private ByteBuffer increaseNextLimit(int step){
+        return this.bytes
+                .limit(this.bytes.position() + step)
+                .mark();
+    }
+
+    private String deserializeProtocolSignature(){
+        return new String(nextBytes(this.bytes, HasdSerializableImpl.SERIALIZATION_PROTOCOL_NAME.getBytes().length));
+    }
+
+    private List<Byte> deserializeProtocolVersion(){
+        List<Byte> res = new ArrayList<>();
+        for (byte b : nextBytes(this.bytes, 3)){
+            res.add(b);
+        }
+        return res;
+    }
+
+    private Integer nextNearestValueLength(){
+        Integer length = (Integer) Varint.decodeToInt(bytes.array(), bytes.position())._1();
+        Integer lengthBytesSize = Varint.extractLength(bytes.array(), bytes.position());
+        this.bytes.position(this.bytes.position() + lengthBytesSize)
+                .mark();
+        return length;
+    }
+
+    private List<Byte> deserializeNullFieldPositions(){
+        Integer nullFieldsLength = nextNearestValueLength();
+        List<Byte> res = new ArrayList<>();
+        for (byte b : nextBytes(this.bytes, nullFieldsLength)){
+            res.add(b);
+        }
+        return res;
+    }
+
+    private String deserializeNextString(){
+        Integer stringLength = nextNearestValueLength();
+        return new String(nextBytes(this.bytes, stringLength));
+    }
+
+    private <T> List<T> deserializeNextList(Class<T> itemClass) {
+        Integer listLength = nextNearestValueLength();
+        //List<T> deserializedList = new
         return null;
     }
-    private static String deserializeProtocolSignature(byte[] bytes){
-        return deserializeProtocolSignature(bytes, PROTOCOL_SIGNATURE_OFFSET);
-    }
 
-    private static List<Integer> deserializeProtocolVersion(byte[] bytes, int offset){
+    private Map<?, ?> deserializeNextMap(ByteBuffer bb) {
         return null;
     }
-    private static List<Integer> deserializeProtocolVersion(byte[] bytes){
-        return deserializeProtocolVersion(bytes, PROCOTOL_VERSION_OFFSET);
-    }
 
-    private static List<Integer> deserializeNullFieldPositions(byte[] bytes, int offset){
+    private HasdSerializable deserializeNextHasdSerializable(ByteBuffer bb) {
         return null;
     }
-    private static List<Integer> deserializeNullFieldPositions(byte[] bytes){
-        return deserializeNullFieldPositions(bytes, NULL_FIELDS_OFFSET);
-    }
 
-    private static Object deserializeValue(Class<?> fieldType, byte[] bytes) throws IllegalAccessException {
-        ByteBuffer buffer = ByteBuffer.wrap(bytes);
-
+    private Object deserializeNextValue(Class<?> fieldType, Field field, Object o) throws IllegalAccessException {
         if (fieldType.equals(boolean.class) || fieldType.equals(Boolean.class)) {
-            return buffer.get() != 0;
+            return nextByte(this.bytes) != 0;
         } else if (fieldType.equals(byte.class) || fieldType.equals(Byte.class)) {
-            return buffer.get();
+            return nextByte(this.bytes);
         } else if (fieldType.equals(short.class) || fieldType.equals(Short.class)) {
-            return buffer.getShort();
+            return increaseNextLimit(Short.BYTES).getShort();
         } else if (fieldType.equals(int.class) || fieldType.equals(Integer.class)) {
-            return buffer.getInt();
+            return increaseNextLimit(Integer.BYTES).getInt();
         } else if (fieldType.equals(long.class) || fieldType.equals(Long.class)) {
-            return buffer.getLong();
+            return increaseNextLimit(Long.BYTES).getLong();
         } else if (fieldType.equals(float.class) || fieldType.equals(Float.class)) {
-            return buffer.getFloat();
+            return increaseNextLimit(Float.BYTES).getFloat();
         } else if (fieldType.equals(double.class) || fieldType.equals(Double.class)) {
-            return buffer.getDouble();
+            return increaseNextLimit(Double.BYTES).getDouble();
         } else if (fieldType.equals(char.class) || fieldType.equals(Character.class)) {
-            return buffer.getChar();
+            return increaseNextLimit(Character.BYTES).getChar();
         } else if (fieldType.equals(String.class)) {
-            int length = (Integer) Varint.decodeToInt(bytes)._1();
-            int offset = Varint.extractLength(bytes, 0);
-            byte[] stringBytes = new byte[length];
-            buffer.get(offset, stringBytes);
-            return new String(stringBytes);
+            return deserializeNextString();
         //} else if (List.class.isAssignableFrom(fieldType)) {
-        //    // Обработка десериализации списка
-        //    deserializedValue = deserializeList(buffer);
+        //    List val = ((List) field.get(o)).;
+        //    return deserializeNextList();
         //} else if (Map.class.isAssignableFrom(fieldType) || HashMap.class.isAssignableFrom(fieldType)) {
-        //    // Обработка десериализации карты
-        //    deserializedValue = deserializeMap(buffer);
+        //    return deserializeMap(buffer);
         //} else if (HasdSerializable.class.isAssignableFrom(fieldType)) {
-        //    // Обработка десериализации пользовательского объекта
-        //    deserializedValue = deserializeCustomObject(buffer, fieldType);
+        //    return deserializeHasdSerializable(buffer);
         } else {
             throw new IllegalArgumentException("Unsupported field type: " + fieldType.getName());
         }
     }
-
-    //private static List<?> deserializeList(ByteBuffer buffer) {
-    //    // Логика десериализации списка
-    //    // Необходимо считать длину списка и затем рекурсивно десериализовать каждый
-    //    // элемент
-    //    return null;
-    //}
-
-    //private static Map<?, ?> deserializeMap(ByteBuffer buffer) {
-    //    // Логика десериализации карты
-    //    // Необходимо считать количество пар ключ-значение и десериализовать их
-    //    return null;
-    //}
-
-    //private static Object deserializeCustomObject(ByteBuffer buffer, Class<?> fieldType) {
-    //    // Логика десериализации пользовательского объекта, реализующего
-    //    // HasdSerializable
-    //    try {
-    //        HasdSerializable obj = (HasdSerializable) fieldType.getDeclaredConstructor().newInstance();
-    //        int length = Varint.decodeInt(buffer);
-    //        byte[] objBytes = new byte[length];
-    //        buffer.get(objBytes);
-    //        return obj.deserialize(objBytes);
-    //    } catch (Exception e) {
-    //        throw new RuntimeException("Failed to deserialize custom object", e);
-    //    }
-    //}
 }
